ffc36d0de7c4caf7a3727e6eb5a9f0a098ec5e9a
madhupriya <100466087+ma594@users.noreply.github.com>
2022-03-21 16:48:25 +0530

    Add files via upload

---
+++ switch.java
@@ -0,0 +1,36 @@
+import java.util.*;
+class Number
+Public Static Void Main(String args[])
+{
+    int day;
+    Scanner s=new Scanner(System.in);
+    System.out.println("enter the day");
+    day=Sc.nextInt();
+    switch(day)||switch variable
+    {
+        case 1:
+            System.out.println("sunday");
+            break;
+            case 2:
+                System.out.println("monday");
+                break;
+                case 3:
+                    System.out.println("tuesday");
+                    break;
+                    case 4:
+                        System.out.println("wednesday");
+                        break;
+                        case 5:
+                            System.out.println("thursday");
+                            break;
+                            case 6:
+                                System.out.println("friday");
+                                break;
+                                case 7:
+                                    System.out.println("saturday");
+                                    break;
+                                    default:
+                                    Sytem.out.println("invalid");
+  }
+} 
+}
