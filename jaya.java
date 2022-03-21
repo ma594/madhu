098facd5e51ca5df526338c08c843283cec5e5b1
madhupriya <100466087+ma594@users.noreply.github.com>
2022-03-21 16:58:17 +0530

    Add files via upload

---
+++ jaya.java
@@ -0,0 +1,16 @@
+import java.util.scanner;
+class jaya
+{
+    public static void main(String args[]);
+    {
+        Scanner s=new Scanner(sytem.in);
+        int num1,num2;
+        System.out.println("System.in");
+        num1=sc.nextInt();
+        System.out.println("System.in");
+        num2=sc.nextint();
+        int sum=num1+num2;
+        System.out.println("the sum of the two numbers is"+sum);
+        
+    }
+}
