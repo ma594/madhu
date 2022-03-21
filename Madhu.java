b401f56b2fe1d33c6d04d8f63f39a12b468a2dcf
madhupriya <100466087+ma594@users.noreply.github.com>
2022-03-21 17:00:38 +0530

    Add files via upload

---
+++ Madhu.java
@@ -0,0 +1,23 @@
+import java.util.scanner;
+class Madhu
+{
+Public static void main(String args[])
+{
+Scanner s=new scanner(System.in);
+int age,weight;
+System.out.println("enter age");
+age=sc.nextInt();
+System.out.println("enter weight");
+weight=sc.nextInt();
+if(age>=18&& weight>=50)
+{
+System.out.println("eligible for blood donation");
+}
+else
+{
+    System.out.println("not eligible for blood donation");
+}
+}
+
+
+
