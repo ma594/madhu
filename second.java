153989c0b85b3e16a226ac89c2f81b978124e38e
madhupriya <100466087+ma594@users.noreply.github.com>
2022-03-21 17:12:50 +0530

    Add files via upload

---
+++ second.java
@@ -0,0 +1,28 @@
+import java.util.scanner;
+Public class Swap
+{
+    Public Static Void Main(String args[])
+    {
+        int max;
+        System.out.println("enter size of an array");
+        n=sc.nextInt();
+        int a[]=newInt();
+        //redaing  elements from user
+        System.out.println("enter array elements");
+        for(int i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+        }
+        //sorting
+        for(int i=0;in;i++)
+        {
+            if(a[i]>a[j])
+            {
+                int temp=a[i];
+                a[i]=a[j];
+                a[j]=temp;
+            }
+        }
+        }
+    }
+}
