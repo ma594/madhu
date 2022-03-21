a58344e794cc18e38b5a0584c93279c9db9c282f
madhupriya <100466087+ma594@users.noreply.github.com>
2022-03-21 17:13:13 +0530

    Add files via upload

---
+++ secondl.java
@@ -0,0 +1,31 @@
+import java.util.scanner;
+public class second largest
+{
+    Public Static Void Main(String args[])
+    {
+       int temp;
+       for(int i=0;i,total;i++)
+        {
+            for(int i=0;i<total;i++)
+            {
+                for(int j=i+1;j<total;j++)
+                {
+                    if(a[i]>a[j])
+                    {
+                        temp=a[i];
+                        a[i]=a[j];
+                        a[j]=temp;
+                    }
+                }
+                    returna[total-2];
+            }
+            System.out.println("second largest:"+getsecondlargest(i));
+                    
+}                    }
+                }
+            }
+        }
+        
+    }
+    
+}
